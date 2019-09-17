611334c4d316e1ae020774ffcca4343c0ae09b41
Tayler Johnson <johnsontayler@me.com>
2019-09-17 20:26:00 -0300

    revised seeds and revised movie year to integer in database

--- db/seeds.rb
+++ db/seeds.rb
@@ -98,27 +98,27 @@ the_dark_knight_rises = Movie.create!(title: 'The Dark Knight Rises', photo: 'ht
                           genre: 'Action', description: 'Eight years after the Jokers reign of anarchy, Batman, with the help of the enigmatic Catwoman, is forced from his exile to save Gotham City, now on the edge of total annihilation, from the brutal guerrilla terrorist Bane.',
                           year: 2012, duration: 164, director: 'Christopher Nolan', imdb_rating: 8.4)
 
-refai = User.create!(first_name: 'Refai', last_name: 'x', email: 'refai@gmail.com', photo: 'https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80')
-tayler = User.create!(first_name: 'Tayler', last_name: 'Johnson', email: 'tayler@gmail.com', photo: 'https://images.unsplash.com/photo-1546538994-4f15d0aa966f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80')
-joseph = User.create!(first_name: 'Joseph', last_name: 'Schemama', email: 'joseph@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSDdQudSzUKlIuQvMZJuEZePygVJ-oiwaWF0XqEMFm_4P3h9ZFH')
-constance = User.create!(first_name: 'Constance', last_name: 'Graftieaux', email: 'constance@gmail.com', photo: 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80')
-anderson = User.create!(first_name: 'Anderson', last_name: 'Costa', email: 'anderson@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTc2BWiA7C9BSG_8TiPxYRvs8R_7Ngdfeo3E-o1rOa25MttraeY')
-andre = User.create!(first_name: 'Andre', last_name: 'Ferrer', email: 'andre@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRXn3gmXdR9fyn0O6r41u-b3g3ZljlxdtaxxdWjmJitpDpDYMMS')
-brittany = User.create!(first_name: 'Brittany', last_name: 'Correll', email: 'brittany@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQGHN8mgodcEbE6H_ARPX6SJ2uCxYCaB68feRjaPV7ahG03a5v6')
-cahue = User.create!(first_name: 'Cahue', last_name: 'Beltrao', email: 'cahue@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS-HGZGUJKAc9pB19NeZr_RLADrtiOhZnJbCTP25bVs-AokWMHf')
-cyrille = User.create!(first_name: 'Cyrille', last_name: 'Labesse', email: 'cyrille@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRfG9-bf6t70gJAT1EWMRyGXwVPxfUhrks9FmycteRDAEE3mVXZ')
-diana = User.create!(first_name: 'Diana', last_name: 'Pavel', email: 'diana@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS5H0TEHtlGq1sKvweB165eENTeFjh_dfvDtttIbik1cQgcr0Rv')
-fernando = User.create!(first_name: 'Fernando', last_name: 'Martins', email: 'fernando@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTwUlgdl1jS3sHbAFwKzBE-z3WPbbU7qwMBT5O4hB9Lq9QC6ncL')
-lucas = User.create!(first_name: 'Lucas', last_name: 'Martins', email: 'lucas@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS6nr_xjkS48VcukH3ydaKt1dLji-wMZuLhvtqJ6nw2zfBg67fM')
-matheus = User.create!(first_name: 'Matheus', last_name: 'Penchel', email: 'matheus@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS25AbBrd_3NmceWNvLfTHTxvrtDxsm0femHA5zPYLdcqZzDV7d')
-milene = User.create!(first_name: 'Milene', last_name: 'Cardoso', email: 'milene@gmail.com', photo: 'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80')
-natalia = User.create!(first_name: 'Natalia', last_name: 'Barthe', email: 'natalia@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQZyNv-Zh4fUKnnulv-AAwnrfWclNMsM-LagDGZgbRFzBY4yZXY')
-wadi = User.create!(first_name: 'Lucas', last_name: 'Wadi', email: 'wadi@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS0_UQAv6wak5s61AqnZCuSNd5x5l0JzvIYR4YU7Yd6S-KrTKTN')
-marcel = User.create!(first_name: 'Marcel', last_name: 'S.F.', email: 'marcel@gmail.com', photo: 'https://www.subphoto.net/sy-photos/2018/10/25/16/small_069d54_2018_Besney_Jonathan-9837.jpg')
-ryan = User.create!(first_name: 'Ryan', last_name: 'Mansfield', email: 'ryan@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSGk_X8hj3pDNoiyCRn3bxGNkEA8cwyMlXoCoyU6eKFXnjoVa4s')
-sarra = User.create!(first_name: 'Sarra', last_name: 'Ghaib', email: 'sarra@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQK24tXyyQTX501kKlabGSGlHsXdaB3FUmh3p9grOsI-dnY_fzk')
-tabata = User.create!(first_name: 'Tabata', last_name: 'Lisboa', email: 'tabata@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR0buXpNw2a8z78DasnOjXPkyCPLSm2oWt3WrHiMIMu6ppKJEeN')
-pedro = User.create!(first_name: 'Pedro', last_name: 'Meyer', email: 'pedro@gmail.com', photo: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80')
+refai = User.create!(first_name: 'Refai', last_name: 'x', email: 'refai@gmail.com', photo: 'https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80', password: 123456)
+tayler = User.create!(first_name: 'Tayler', last_name: 'Johnson', email: 'tayler@gmail.com', photo: 'https://images.unsplash.com/photo-1546538994-4f15d0aa966f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80', password: 123456)
+joseph = User.create!(first_name: 'Joseph', last_name: 'Schemama', email: 'joseph@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSDdQudSzUKlIuQvMZJuEZePygVJ-oiwaWF0XqEMFm_4P3h9ZFH', password: 123456)
+constance = User.create!(first_name: 'Constance', last_name: 'Graftieaux', email: 'constance@gmail.com', photo: 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80', password: 123456)
+anderson = User.create!(first_name: 'Anderson', last_name: 'Costa', email: 'anderson@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTc2BWiA7C9BSG_8TiPxYRvs8R_7Ngdfeo3E-o1rOa25MttraeY', password: 123456)
+andre = User.create!(first_name: 'Andre', last_name: 'Ferrer', email: 'andre@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRXn3gmXdR9fyn0O6r41u-b3g3ZljlxdtaxxdWjmJitpDpDYMMS', password: 123456)
+brittany = User.create!(first_name: 'Brittany', last_name: 'Correll', email: 'brittany@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQGHN8mgodcEbE6H_ARPX6SJ2uCxYCaB68feRjaPV7ahG03a5v6', password: 123456)
+cahue = User.create!(first_name: 'Cahue', last_name: 'Beltrao', email: 'cahue@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS-HGZGUJKAc9pB19NeZr_RLADrtiOhZnJbCTP25bVs-AokWMHf', password: 123456)
+cyrille = User.create!(first_name: 'Cyrille', last_name: 'Labesse', email: 'cyrille@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRfG9-bf6t70gJAT1EWMRyGXwVPxfUhrks9FmycteRDAEE3mVXZ', password: 123456)
+diana = User.create!(first_name: 'Diana', last_name: 'Pavel', email: 'diana@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS5H0TEHtlGq1sKvweB165eENTeFjh_dfvDtttIbik1cQgcr0Rv', password: 123456)
+fernando = User.create!(first_name: 'Fernando', last_name: 'Martins', email: 'fernando@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTwUlgdl1jS3sHbAFwKzBE-z3WPbbU7qwMBT5O4hB9Lq9QC6ncL', password: 123456)
+lucas = User.create!(first_name: 'Lucas', last_name: 'Martins', email: 'lucas@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS6nr_xjkS48VcukH3ydaKt1dLji-wMZuLhvtqJ6nw2zfBg67fM', password: 123456)
+matheus = User.create!(first_name: 'Matheus', last_name: 'Penchel', email: 'matheus@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS25AbBrd_3NmceWNvLfTHTxvrtDxsm0femHA5zPYLdcqZzDV7d', password: 123456)
+milene = User.create!(first_name: 'Milene', last_name: 'Cardoso', email: 'milene@gmail.com', photo: 'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80', password: 123456)
+natalia = User.create!(first_name: 'Natalia', last_name: 'Barthe', email: 'natalia@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQZyNv-Zh4fUKnnulv-AAwnrfWclNMsM-LagDGZgbRFzBY4yZXY', password: 123456)
+wadi = User.create!(first_name: 'Lucas', last_name: 'Wadi', email: 'wadi@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS0_UQAv6wak5s61AqnZCuSNd5x5l0JzvIYR4YU7Yd6S-KrTKTN', password: 123456)
+marcel = User.create!(first_name: 'Marcel', last_name: 'S.F.', email: 'marcel@gmail.com', photo: 'https://www.subphoto.net/sy-photos/2018/10/25/16/small_069d54_2018_Besney_Jonathan-9837.jpg', password: 123456)
+ryan = User.create!(first_name: 'Ryan', last_name: 'Mansfield', email: 'ryan@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSGk_X8hj3pDNoiyCRn3bxGNkEA8cwyMlXoCoyU6eKFXnjoVa4s', password: 123456)
+sarra = User.create!(first_name: 'Sarra', last_name: 'Ghaib', email: 'sarra@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQK24tXyyQTX501kKlabGSGlHsXdaB3FUmh3p9grOsI-dnY_fzk', password: 123456)
+tabata = User.create!(first_name: 'Tabata', last_name: 'Lisboa', email: 'tabata@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR0buXpNw2a8z78DasnOjXPkyCPLSm2oWt3WrHiMIMu6ppKJEeN', password: 123456)
+pedro = User.create!(first_name: 'Pedro', last_name: 'Meyer', email: 'pedro@gmail.com', photo: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80', password: 123456)
 
 refai_taste = Taste.create!(movie: godfather, user: refai,
                             watched: true, rating: true)
@@ -161,7 +161,8 @@ sarra_taste = Taste.create!(movie: whiplash, user: sarra,
 tabata_taste = Taste.create!(movie: the_prestige, user: tabata,
                              watched: true, rating: true)
 pedro_taste = Taste.create!(movie: django_unchained, user: pedro,
-                            watched: true, rating: true)godfather = Movie.create!(title: 'The Godfather', photo: 'https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,704,1000_AL_.jpg',
+                            watched: true, rating: true)
+godfather = Movie.create!(title: 'The Godfather', photo: 'https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,704,1000_AL_.jpg',
                           genre: 'Crime', description: 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.',
                           year: 1972, duration: 175, director: 'Francis Ford Coppola', imdb_rating: 9.1)
 
@@ -252,68 +253,3 @@ django_unchained = Movie.create!(title: 'Django Unchained', photo: 'https://m.me
 the_dark_knight_rises = Movie.create!(title: 'The Dark Knight Rises', photo: 'https://m.media-amazon.com/images/M/MV5BMTk4ODQzNDY3Ml5BMl5BanBnXkFtZTcwODA0NTM4Nw@@._V1_UX182_CR0,0,182,268_AL_.jpg',
                           genre: 'Action', description: 'Eight years after the Jokers reign of anarchy, Batman, with the help of the enigmatic Catwoman, is forced from his exile to save Gotham City, now on the edge of total annihilation, from the brutal guerrilla terrorist Bane.',
                           year: 2012, duration: 164, director: 'Christopher Nolan', imdb_rating: 8.4)
-
-refai = User.create!(first_name: 'Refai', last_name: 'x', email: 'refai@gmail.com', photo: 'https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80')
-tayler = User.create!(first_name: 'Tayler', last_name: 'Johnson', email: 'tayler@gmail.com', photo: 'https://images.unsplash.com/photo-1546538994-4f15d0aa966f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80')
-joseph = User.create!(first_name: 'Joseph', last_name: 'Schemama', email: 'joseph@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSDdQudSzUKlIuQvMZJuEZePygVJ-oiwaWF0XqEMFm_4P3h9ZFH')
-constance = User.create!(first_name: 'Constance', last_name: 'Graftieaux', email: 'constance@gmail.com', photo: 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80')
-anderson = User.create!(first_name: 'Anderson', last_name: 'Costa', email: 'anderson@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTc2BWiA7C9BSG_8TiPxYRvs8R_7Ngdfeo3E-o1rOa25MttraeY')
-andre = User.create!(first_name: 'Andre', last_name: 'Ferrer', email: 'andre@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRXn3gmXdR9fyn0O6r41u-b3g3ZljlxdtaxxdWjmJitpDpDYMMS')
-brittany = User.create!(first_name: 'Brittany', last_name: 'Correll', email: 'brittany@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQGHN8mgodcEbE6H_ARPX6SJ2uCxYCaB68feRjaPV7ahG03a5v6')
-cahue = User.create!(first_name: 'Cahue', last_name: 'Beltrao', email: 'cahue@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS-HGZGUJKAc9pB19NeZr_RLADrtiOhZnJbCTP25bVs-AokWMHf')
-cyrille = User.create!(first_name: 'Cyrille', last_name: 'Labesse', email: 'cyrille@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRfG9-bf6t70gJAT1EWMRyGXwVPxfUhrks9FmycteRDAEE3mVXZ')
-diana = User.create!(first_name: 'Diana', last_name: 'Pavel', email: 'diana@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS5H0TEHtlGq1sKvweB165eENTeFjh_dfvDtttIbik1cQgcr0Rv')
-fernando = User.create!(first_name: 'Fernando', last_name: 'Martins', email: 'fernando@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTwUlgdl1jS3sHbAFwKzBE-z3WPbbU7qwMBT5O4hB9Lq9QC6ncL')
-lucas = User.create!(first_name: 'Lucas', last_name: 'Martins', email: 'lucas@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS6nr_xjkS48VcukH3ydaKt1dLji-wMZuLhvtqJ6nw2zfBg67fM')
-matheus = User.create!(first_name: 'Matheus', last_name: 'Penchel', email: 'matheus@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS25AbBrd_3NmceWNvLfTHTxvrtDxsm0femHA5zPYLdcqZzDV7d')
-milene = User.create!(first_name: 'Milene', last_name: 'Cardoso', email: 'milene@gmail.com', photo: 'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80')
-natalia = User.create!(first_name: 'Natalia', last_name: 'Barthe', email: 'natalia@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQZyNv-Zh4fUKnnulv-AAwnrfWclNMsM-LagDGZgbRFzBY4yZXY')
-wadi = User.create!(first_name: 'Lucas', last_name: 'Wadi', email: 'wadi@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS0_UQAv6wak5s61AqnZCuSNd5x5l0JzvIYR4YU7Yd6S-KrTKTN')
-marcel = User.create!(first_name: 'Marcel', last_name: 'S.F.', email: 'marcel@gmail.com', photo: 'https://www.subphoto.net/sy-photos/2018/10/25/16/small_069d54_2018_Besney_Jonathan-9837.jpg')
-ryan = User.create!(first_name: 'Ryan', last_name: 'Mansfield', email: 'ryan@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSGk_X8hj3pDNoiyCRn3bxGNkEA8cwyMlXoCoyU6eKFXnjoVa4s')
-sarra = User.create!(first_name: 'Sarra', last_name: 'Ghaib', email: 'sarra@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQK24tXyyQTX501kKlabGSGlHsXdaB3FUmh3p9grOsI-dnY_fzk')
-tabata = User.create!(first_name: 'Tabata', last_name: 'Lisboa', email: 'tabata@gmail.com', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR0buXpNw2a8z78DasnOjXPkyCPLSm2oWt3WrHiMIMu6ppKJEeN')
-pedro = User.create!(first_name: 'Pedro', last_name: 'Meyer', email: 'pedro@gmail.com', photo: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80')
-
-refai_taste = Taste.create!(movie: godfather, user: refai,
-                            watched: true, rating: true)
-tayler_taste = Taste.create!(movie: godfather_2, user: tayler,
-                             watched: true, rating: true)
-joseph_taste = Taste.create!(movie: shawshank, user: joseph,
-                             watched: true, rating: true)
-constance_taste = Taste.create!(movie: dark_knight, user: constance,
-                                watched: true, rating: true)
-anderson_taste = Taste.create!(movie: pulp_fiction, user: anderson,
-                               watched: true, rating: true)
-andre_taste = Taste.create!(movie: fight_club, user: andre,
-                            watched: true, rating: true)
-brittany_taste = Taste.create!(movie: inception, user: brittany,
-                               watched: true, rating: true)
-cahue_taste = Taste.create!(movie: matrix, user: cahue,
-                            watched: true, rating: true)
-cyrille_taste = Taste.create!(movie: goodfellas, user: cyrille,
-                              watched: true, rating: true)
-diana_taste = Taste.create!(movie: se7en, user: diana,
-                            watched: true, rating: true)
-fernando_taste = Taste.create!(movie: city_of_god, user: fernando,
-                               watched: true, rating: true)
-lucas_taste = Taste.create!(movie: the_silence_of_the_lamb, user: lucas,
-                            watched: true, rating: true)
-matheus_taste = Taste.create!(movie: the_usual_suspect, user: matheus,
-                              watched: true, rating: true)
-milene_taste = Taste.create!(movie: the_lion_king, user: milene,
-                             watched: true, rating: true)
-natalia_taste = Taste.create!(movie: american_history_x, user: natalia,
-                              watched: true, rating: true)
-wadi_taste = Taste.create!(movie: untouchable, user: wadi,
-                           watched: true, rating: true)
-marcel_taste = Taste.create!(movie: gladiator, user: marcel,
-                             watched: true, rating: true)
-ryan_taste = Taste.create!(movie: the_departed, user: ryan,
-                           watched: true, rating: true)
-sarra_taste = Taste.create!(movie: whiplash, user: sarra,
-                            watched: true, rating: true)
-tabata_taste = Taste.create!(movie: the_prestige, user: tabata,
-                             watched: true, rating: true)
-pedro_taste = Taste.create!(movie: django_unchained, user: pedro,
-                            watched: true, rating: true)
