6b07dc54b1c198c0af7cb18005a0f0a9352f6cae
Constance Graftieaux <constance.graftieaux@gmail.com>
2019-09-18 16:01:25 -0300

    backend of the search bar

---
+++ app/assets/javascripts/search_bar.js
@@ -0,0 +1,22 @@
+const results = document.querySelector("#results");
+const searchQuery = document.getElementById("search_query")
+
+searchQuery.addEventListener("keyup", event => {
+  if(event.currentTarget.value == ""){
+    results.innerHTML = ""
+  } else {
+  fetch(`/users.json?utf8=✓&query=${event.currentTarget.value}&button=`)
+  .then(response => response.json())
+  .then((data) => {
+    results.innerHTML = ""
+    data.forEach((result) => {
+      const user = `<li class="list-inline-item">
+        <img src="${result.photo}" alt="" class="avatar">
+        ${result.first_name}
+        ${result.last_name}
+      </li> <br>`;
+      results.insertAdjacentHTML("beforeend", user);
+    });
+  });
+  }
+});
--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,7 @@
 class DashboardsController < ApplicationController
+  skip_after_action :verify_policy_scoped
 
   def index
-    @users = policy_scope(User).all
+
   end
 end
