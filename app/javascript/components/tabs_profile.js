a7404c3ba5af754b216e32aff61d97e8ca1735b6
Tayler Johnson <johnsontayler@me.com>
2019-09-18 15:09:35 -0300

    profile tab underline javascript works

--- app/javascript/components/tabs_profile.js
+++ app/javascript/components/tabs_profile.js
@@ -3,18 +3,14 @@ const showTabContentOnClick = () => {
   const wishlistTab = document.getElementById('wishlist-tab');
   const watchedContent = document.getElementById('watched');
   const wishlistContent = document.getElementById('wishlist');
-  let lavaLamp = document.getElementById("myTab");
-
-  console.log(lavaLamp);
+  const myTab = document.getElementById("myTab");
 
   watchedTab.addEventListener('click', () => {
     wishlistTab.classList.remove('active');
     wishlistContent.classList.remove('active', 'show');
     watchedTab.classList.add('active');
     watchedContent.classList.add('active', 'show');
-    lavaLamp.firstChildElement = `<div class="lavalamp-object ease"
-    style="transition-duration: 0.2s; width: 128.141px;
-    height: 40px; transform: translate(0px, 0px);"></div>`;
+    myTab.querySelector('div:first-child').style = "transition-duration: 0.2s; width: 128.141px; height: 40px; transform: translate(0px, 0px)";
   });
 
   wishlistTab.addEventListener('click', () => {
@@ -22,9 +18,7 @@ const showTabContentOnClick = () => {
     watchedContent.classList.remove('active', 'show');
     wishlistTab.classList.add('active');
     wishlistContent.classList.add('active', 'show');
-    lavaLamp.firstChildElement = `<div class="lavalamp-object ease"
-    style="transition-duration: 0.2s; width: 139.656px;
-    height: 40px; transform: translate(148.141px, 0px);"></div>`;
+    myTab.querySelector('div:first-child').style = "transition-duration: 0.2s; width: 128.141px; height: 40px; transform: translate(148.141px, 0px)";
   });
 };
 
