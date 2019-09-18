b9ec69878aa08d61759bcce94d5cefccb5317a79
Tayler Johnson <johnsontayler@me.com>
2019-09-17 22:15:44 -0300

    watched by method semi complete. code wills with users who have watched the movie. next step will be to refine it to only people that the user follows. and the carousel swipe the avatars

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,6 +6,7 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
     authorize @movie
-    @taste = Taste.where(user: current_user, movie: @movie)
+    @user_taste = Taste.where(user: current_user, movie: @movie)
+    @watched_by = Taste.where(movie: @movie, watched: true)
   end
 end
