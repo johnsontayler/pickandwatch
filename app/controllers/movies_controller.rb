61fb38c607c23bd6baf70a581f0919edf90ddc6d
Tayler Johnson <johnsontayler@me.com>
2019-09-17 21:51:55 -0300

    movie wish list bookmark empty if taste is empty and filled if taste exists. movie photo and description complete too

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,5 +6,6 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
     authorize @movie
+    @taste = Taste.where(user: current_user, movie: @movie)
   end
 end
