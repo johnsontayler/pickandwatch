349de716899bea6c5626ffe570c080d8cf3c7474
Tayler Johnson <johnsontayler@me.com>
2019-09-18 14:50:19 -0300

    tabs design and backend complete minus orange underline javascript

--- app/javascript/components/tabs_profile.js
+++ app/javascript/components/tabs_profile.js
@@ -1,17 +1,30 @@
 const showTabContentOnClick = () => {
   const watchedTab = document.getElementById('watched-tab');
   const wishlistTab = document.getElementById('wishlist-tab');
-  const watched = document.getElementById('watched');
-  const wishlist = document.getElementById('wishlist');
+  const watchedContent = document.getElementById('watched');
+  const wishlistContent = document.getElementById('wishlist');
+  let lavaLamp = document.getElementById("myTab");
+
+  console.log(lavaLamp);
 
   watchedTab.addEventListener('click', () => {
-    watched.classList.remove('tab-pane fade');
-    watched.classList.add('tab-pane fade active show');
+    wishlistTab.classList.remove('active');
+    wishlistContent.classList.remove('active', 'show');
+    watchedTab.classList.add('active');
+    watchedContent.classList.add('active', 'show');
+    lavaLamp.firstChildElement = `<div class="lavalamp-object ease"
+    style="transition-duration: 0.2s; width: 128.141px;
+    height: 40px; transform: translate(0px, 0px);"></div>`;
   });
 
   wishlistTab.addEventListener('click', () => {
-    wishlist.classList.remove('tab-pane fade');
-    wishlist.classList.add('tab-pane fade active show');
+    watchedTab.classList.remove('active');
+    watchedContent.classList.remove('active', 'show');
+    wishlistTab.classList.add('active');
+    wishlistContent.classList.add('active', 'show');
+    lavaLamp.firstChildElement = `<div class="lavalamp-object ease"
+    style="transition-duration: 0.2s; width: 139.656px;
+    height: 40px; transform: translate(148.141px, 0px);"></div>`;
   });
 };
 
