a19b992c612c02d563f8d4a44030db3f6b4fb018
Tayler Johnson <johnsontayler@me.com>
2019-09-19 14:19:08 -0300

    unnested the follows routes from users bc wasn't necessary

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :users, only: [:index, :show] do
-    resources :follows, only: [:create, :destroy]
-  end
+  resources :users, only: [:index, :show]
+
+  resources :follows, only: [:create, :destroy]
 
   resources :movies, only: [:index, :show] do
     resources :tastes, only: [:create]
