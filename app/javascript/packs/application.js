b08af9de33ca12616fb2ecbd2d6e0fab2df3771c
Tayler Johnson <johnsontayler@me.com>
2019-09-23 18:27:44 -0300

    dashboard done. nested watch like and bookmark under taste route. ajax for bookmark icon. nostyle on all links

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -38,11 +38,14 @@ dislike.addEventListener('click', (event) => {
 }
 // END
 
+
+
 import { showTabContentOnClick } from '../components/tabs_profile';
 import { shuffleMovie } from '../components/movie_shuffle';
 import { fetchMovies } from './movies';
 import { filterCategories } from '../components/filter_categories';
 import { searchBar } from '../components/search_bar';
+import { initBookmarks } from '../components/bookmark';
 // import { followButton } from '../components/follow_button_profile';
 
 const input = document.querySelector('#search');
@@ -56,3 +59,4 @@ const movieIndex = document.querySelector('#search');
 showTabContentOnClick();
 filterCategories();
 searchBar();
+initBookmarks();
