ac1917199508ffcaf2c939a7c78b84a973a79e65
Joseph Scemama <j.scemama1@gmail.com>
2019-09-25 15:50:59 -0300

    start trying without success

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module Pickandwatch
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
