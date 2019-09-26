fb1887ae97fcd22cb9c799b64e1839e490859ef3
Joseph Scemama <j.scemama1@gmail.com>
2019-09-26 12:07:06 -0300

    New navbar with animation, just need to change colors

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -48,6 +48,7 @@ import { fetchMovies } from './movies';
 import { filterCategories } from '../components/filter_categories';
 import { searchBar } from '../components/search_bar';
 import { initBookmarks } from '../components/bookmark';
+import { animations } from '../components/animations';
 import { requireRating } from './movies';
 import { searchBarShuffle } from './shuffle_with_friend';
 import { shuffleFormButtons } from './shuffle_with_friend';
@@ -66,6 +67,7 @@ filterCategories();
 searchBar();
 initBookmarks();
 searchBarShuffle();
+animations();
 
 
 
